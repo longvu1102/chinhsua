df69acb7502f74515f2ef8606d3b9800c448480a
unknown <vul7896@gmail.com>
2022-12-23 16:04:30 +0700

    Add new project food

--- index.js
+++ index.js
@@ -1,23 +1,2 @@
-function gethah(cousre){
-    return {
-        id : cousre.id,
-        name: `Khoa hoc : ${cousre.name}`
 
-    }
-}
-
-var cousre = [
-    {
-        id : 1,
-        name : 'javahaha'
-    },
-    {
-        id :2 ,
-        name : 'java'
-    }
-]
-var newCousre = cousre.map(gethah);
-
-
-console.log(newCousre);
 
