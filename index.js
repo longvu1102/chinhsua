96af6f903f553767b87cef75441dc23b9f5a0109
vul7896@gmail.com <vul7896@gmail.com>
2023-01-30 10:25:11 +0700

    first

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+Array.prototype.map2 = function(callback){
+    var arrayLength = this.length
+    for(i = 0 ; i<arrayLength;++i){
 
+    }
+    course =[
+        'java',
+        'php',
+        'html'
+    ]
+    console.log.map2(function(course,index) {
+        console.log(index,course)
+    })
+}
 
